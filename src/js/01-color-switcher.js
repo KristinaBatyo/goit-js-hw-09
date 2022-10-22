dae4bcbf070810a0ebff34c3360844af290dd19f
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-10-22 16:47:53 +0300

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+start.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        body.style.backgroundColor = color;
+    }, 1000)
+    start.disabled = true;
+    stop.disabled = false;
+});
+
+stop.addEventListener('click', () => {
+    clearInterval(timerId);
+    start.disabled = false;
+    stop.disabled = true;
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
